6a28fd91ca4a5090d2d2e1171ede95828ef19602
Rhu-Cr <116230605+Rhu-Cr@users.noreply.github.com>
2022-10-20 00:45:29 +0000

    primeiros arquivos!

---
+++ main.py
@@ -0,0 +1,13 @@
+# Meu primeiro projeto Python!!! 
+#
+# print() = comando de saida
+print("Alo mundo!")
+#quando quiser guardar uma frase(string) 
+nome="Rhuan Cristofolini"
+idade = 21
+
+
+''' 
+Comentario em bloco
+
+'''
