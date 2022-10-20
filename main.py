1867df507519d767bdc1117fa0ea2ddaa1505210
Rhu-Cr <116230605+Rhu-Cr@users.noreply.github.com>
2022-10-20 00:53:17 +0000

    aprendi a criar e exibir variaveis na tela

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 #
 # print() = comando de saida
 print("Alo mundo!")
+
 #quando quiser guardar uma frase(string) 
 nome="Rhuan Cristofolini"
+#quando eu quiser guardar um numero inteiro
 idade = 21
+#quando eu quiser exibir meu nome na variavel nome
+print(nome)
 
-
+#exibir minha idade na variavel idade 
+print(idade)
 ''' 
 Comentario em bloco
 
