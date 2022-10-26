2ab16e111d426b8e49b357eda968d58cbccd3297
Rhu-Cr <116230605+Rhu-Cr@users.noreply.github.com>
2022-10-26 23:13:23 +0000

    comando input, diferença de str e int

---
+++ Aula 1 2022-19-10.py
@@ -0,0 +1,23 @@
+# Meu primeiro projeto Python!!! 
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+#quando quiser guardar uma frase(string) 
+nome="Rhuan Cristofolini"
+#quando eu quiser guardar um numero inteiro
+idade = 21
+#quando eu quiser exibir meu nome na variavel nome
+print(nome)
+
+#exibir a frase minha idade é completando com o conteudo na variavel idade 
+print("minha idade é" +str (idade)+"anos")
+print(f"minha idade é {idade}\n")
+print("minha idade é{}".format(idade))
+#quando quiser exibir "meu nome é... e tenho... anos" trocando pelas variaveis nome e idade 
+print("meu nome é {} e tenho {} anos".format(nome, idade))
+
+''' 
+Comentario em bloco
+
+'''
--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-# Meu primeiro projeto Python!!! 
-#
-# print() = comando de saida
-print("Alo mundo!")
+#comando imput() para permitir que o usuário digite algo
+nome = input('digite seu nome:')
+#comando de saída...exibir na tela
+print(f'Boa noite, seu nome é {nome}')
+#exiba, sua idade é...
+idade = int(input("Digite sua idade: \n"))
+print("Seu nome é "+nome+" e sua idade é "+str(idade))
 
-#quando quiser guardar uma frase(string) 
-nome="Rhuan Cristofolini"
-#quando eu quiser guardar um numero inteiro
-idade = 21
-#quando eu quiser exibir meu nome na variavel nome
-print(nome)
+# e se eu quisesse mostrar o dobro da idade informada
+dobro = idade*2
+print('o dobro da sua idade é:{}'.format(dobro))
 
-#exibir a frase minha idade é completando com o conteudo na variavel idade 
-print("minha idade é" +str (idade)+"anos")
-print(f"minha idade é {idade}\n")
-print("minha idade é{}".format(idade))
-#quando quiser exibir "meu nome é... e tenho... anos" trocando pelas variaveis nome e idade 
-print("meu nome é {} e tenho {} anos".format(nome, idade))
-
-''' 
-Comentario em bloco
-
-'''
